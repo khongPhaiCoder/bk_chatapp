e511cce8a1bf731d687f91ffd8435706d217745a
khongPhaiCoder <soiamhon@gmail.com>
2022-11-20 09:57:30 +0700

    implementing firebase authentication

---
+++ lib/providers/authentication_provider.dart
@@ -0,0 +1,71 @@
+// Packages
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get_it/get_it.dart';
+
+// Models
+import '../models/chat_user.dart';
+
+// Services
+import '../services/database_service.dart';
+import '../services/navigation_service.dart';
+
+// Utils
+import '../utils/contains.dart';
+
+class AuthenticationProvider extends ChangeNotifier {
+  late final FirebaseAuth _auth;
+  late final NavigationService _navigationService;
+  late final DatabaseService _databaseService;
+
+  late ChatUser user;
+
+  AuthenticationProvider() {
+    _auth = FirebaseAuth.instance;
+    _navigationService = GetIt.instance.get<NavigationService>();
+    _databaseService = GetIt.instance.get<DatabaseService>();
+
+    _auth.authStateChanges().listen((_user) {
+      if (_user != null) {
+        _databaseService.updateUserLastSeenTime(_user.uid);
+        _databaseService.getUser(_user.uid).then(
+          (_snapshot) {
+            Map<String, dynamic> _userData =
+                _snapshot.data()! as Map<String, dynamic>;
+            user = ChatUser.fromJSON(
+              {
+                "uid": _user.uid,
+                "name": _userData["name"],
+                "email": _userData["email"],
+                "last_active": _userData["last_active"],
+                "image": _userData["image"]
+              },
+            );
+            _navigationService.removeAndNavigateToRoute(ROUTES["HOME"]!);
+          },
+        );
+      } else {
+        _navigationService.removeAndNavigateToRoute(ROUTES["LOGIN"]!);
+      }
+    });
+  }
+
+  Future<void> loginUsingEmailAndPassword(
+    String _email,
+    String _password,
+  ) async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: _email,
+        password: _password,
+      );
+      print(_auth.currentUser);
+    } on FirebaseAuthException {
+      print("Error logging user into Firebase");
+    } catch (e) {
+      print(e);
+    }
+  }
+}
--- lib/utils/contains.dart
+++ lib/utils/contains.dart
@@ -5,5 +5,10 @@ const String INPUT_FIELD_ERROR_MESSAGE = "Enter a valid value.";
 const REG_EXP = {
   "EMAIL":
       r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
-  "PASSWORD": r".{8,}"
+  "PASSWORD": r".{8,}",
+};
+
+const ROUTES = {
+  "HOME": "/home",
+  "LOGIN": "/login",
 };
